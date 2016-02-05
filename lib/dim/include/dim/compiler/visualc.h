c3ae511e6e6c18af0a12400d719c0ff3bb115d5d
gknowles <gknowles@ieee.org>
2016-02-05 10:14:30 -0800

    Add commented out directives to explicitly set structure packing

--- lib/dim/include/dim/compiler/visualc.h
+++ lib/dim/include/dim/compiler/visualc.h
@@ -4,6 +4,10 @@
 
 #pragma warning(disable: 4100) // unreferenced formal parameter
 
+// #pragma warning(disable: 4103) // alignment changed after including header
+// #pragma warning(disable: 4366) // the result of the unary '&' operator may be unaligned
+// #pragma pack(1)
+
 #define _NO_LOCALES 0
 #define _ITERATOR_DEBUG_LEVEL 0
 
