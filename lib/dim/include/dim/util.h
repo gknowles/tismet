b792f2846b70d7508ac2e58322eb4d30a505ba74
gknowles <gknowles@ieee.org>
2016-04-20 23:58:01 -0700

    Update IntegralStr to avoid signed/unsigned warnings

--- lib/dim/include/dim/util.h
+++ lib/dim/include/dim/util.h
@@ -6,6 +6,7 @@
 namespace Dim {
 
 #include <climits>
+#include <type_traits>
 
 
 /****************************************************************************
@@ -35,28 +36,73 @@ constexpr int maxIntegralChars () {
 
 template <typename T>
 class IntegralStr {
-    char data[maxIntegralChars<T>() + 1];
 public:
     IntegralStr (T val);
     const char * set (T val);
     operator const char * () const;
+
+private:
+    using Signed = typename std::make_signed<T>::type;
+    using Unsigned = typename std::make_unsigned<T>::type;
+
+    const char * internalSet (Signed val);
+    const char * internalSet (Unsigned val);
+
+    char data[maxIntegralChars<T>() + 1];
 };
 
 //===========================================================================
 template <typename T>
 IntegralStr<T>::IntegralStr (T val) { 
-    set(val); 
+    internalSet(val); 
 }
 
 //===========================================================================
 template <typename T>
 const char * IntegralStr<T>::set (T val) {
+    return internalSet(val);
+}
+
+//===========================================================================
+template <typename T>
+IntegralStr<T>::operator const char * () const { 
+    return data; 
+}
+
+//===========================================================================
+template <typename T>
+const char * IntegralStr<T>::internalSet (Unsigned val) {
     if (!val) {
         data[0] = '0';
         data[1] = 0;
     } else {
         char * ptr = data;
-        if (numeric_limits<T>::is_signed && val < 0) {
+        unsigned i = 0;
+        for (;;) {
+            ptr[i] = (val % 10) + '0';
+            val /= 10;
+            i += 1;
+            if (!val)
+                break;
+        }
+        ptr[i] = 0;
+        for (; i > 1; i -= 2) {
+            swap(*ptr, ptr[i]);
+            ptr += 1;
+        }
+    }
+    return data;
+}
+
+//===========================================================================
+template <typename T>
+const char * IntegralStr<T>::internalSet (Signed val) {
+    if (!val) {
+        data[0] = '0';
+        data[1] = 0;
+    } else {
+        char * ptr = data;
+        if (val < 0) {
             *ptr++ = '-';
             val = -val;
         }
@@ -77,12 +123,6 @@ const char * IntegralStr<T>::set (T val) {
     return data;
 }
 
-//===========================================================================
-template <typename T>
-IntegralStr<T>::operator const char * () const { 
-    return data; 
-}
-
 
 /****************************************************************************
 *
@@ -92,6 +132,7 @@ IntegralStr<T>::operator const char * () const {
 
 template <typename T>
 class ForwardListIterator {
+protected:
     T * m_current{nullptr};
 public:
     ForwardListIterator (T * node) : m_current(node) {}
