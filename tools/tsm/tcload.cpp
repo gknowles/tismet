e959c6b3586f8b19cf835b8895144a048a5a4bbd
Glen Knowles <gknowles@ieee.org>
2017-10-03 02:33:46 -0700

    Add --truncate option to "tsm load"

--- tools/tsm/tcload.cpp
+++ tools/tsm/tcload.cpp
@@ -24,6 +24,8 @@ static auto & s_dat = s_cli.opt<Path>("[dat file]")
     .desc("Target database");
 static auto & s_in = s_cli.opt<Path>("[input file]")
     .desc("File to load (default extension: .txt)");
+static auto & s_truncate = s_cli.opt<bool>("truncate", false)
+    .desc("Completely replace database contents");
 
 
 /****************************************************************************
@@ -75,7 +77,11 @@ static bool loadCmd(Cli & cli) {
     if (!s_dat)
         return cli.badUsage("No value given for <dat file[.dat]>");
 
-    auto h = tsdOpen(s_dat->defaultExt("dat"));
+    s_dat->defaultExt("dat");
+    if (s_truncate)
+        fileRemove(*s_dat);
+    auto h = tsdOpen(*s_dat);
+
     auto progress = make_unique<LoadProgress>(h);
     tsdLoadDump(progress.release(), h, s_in->defaultExt("txt"));
     return true;
