124aeacdff3c438867bae11806353a2bf071a2ce
gknowles <gknowles@ieee.org>
2024-07-12 08:58:47 -0700

    Update dimapp & dimcli

--- tools/tsm/tcgen.cpp
+++ tools/tsm/tcgen.cpp
@@ -120,7 +120,7 @@ MetricSource::MetricSource()
         "zero.", "one.", "two.", "three.", "four.",
         "five.", "six.", "seven.", "eight.", "nine.",
     };
-    ToCharsBuf<unsigned> str;
+    ToCharsBuf<unsigned, 10> str;
     for (unsigned i = 0; i < s_opts.metrics; ++i) {
         auto & met = m_metrics[i];
         if (!s_opts.prefix.empty())
